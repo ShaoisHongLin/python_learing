4bbc9fef793eba0ead4a2cc6b362c845229c5e50
shaohl <shao1417047661@gmail.com>
2024-04-23 01:10:28 +0800

    add my39-49.py to list folder

---
+++ chapter03-sequence/list/my49.py
@@ -0,0 +1,17 @@
+# 讲解多维列表。
+a=[
+	["A",18,85],
+	["B",19,90],
+	["C",19,91],
+]
+# a[0][1]也就是A
+
+# 循环打印，用到两个for循环
+'''
+for m in range(3)
+	for n in range(3):
+		print(a[m][n])
+'''
+for m in range(3):
+	for n in range(3):
+		print(a[m][n])
