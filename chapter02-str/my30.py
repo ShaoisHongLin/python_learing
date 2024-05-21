e44f60638e7446a58576c2206e85c87263930639
shaohl <shao1417047661@gmail.com>
2024-05-21 13:58:22 +0800

    my30.py update

--- chapter02-str/my30.py
+++ chapter02-str/my30.py
@@ -9,3 +9,8 @@ print(b)
 # 2.split()可以规定按什么字符划分,但是一般还是常用上面的按空白切割
 print(a.split('be'))
 # ['to','or not to','']
+
+
+问：python的str.split('')的返回值是什么类型？
+
+答：python常见的str.split('')返回值是经过split分割后产生的字符串列表，是列表。
