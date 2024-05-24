443f0c90b885491f0bda5defbec516642994a0d8
shaohl <shao1417047661@gmail.com>
2024-05-24 22:51:59 +0800

    reviewed _33str_functions_that_often_use.py

--- chapter02-str/_33str_functions_that_often_use.py
+++ chapter02-str/_33str_functions_that_often_use.py
@@ -5,6 +5,6 @@ len(a)
 a.startswith('')   # 是否以`内容`开头
 a.endswith('')   # 是否以`内容`结尾
 a.find('')    # 查第一次出现
-a.rfint('')   # 最后一次出现
+a.rfind('')   # 最后一次出现
 a.count('')   # `内容`出现了几次
-a.isalnum()   # 是否都是数字或字符
+a.isalnum()   # 是否都是数字或字符is all num
