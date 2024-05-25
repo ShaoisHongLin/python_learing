82bc180aed551b618f6e8f908296785be08deb27
shaohl <shao1417047661@gmail.com>
2024-05-25 23:38:15 +0800

    reviewed _44list_function_often_use.py

--- chapter03-sequence/list/_44list_function_often_use.py
+++ chapter03-sequence/list/_44list_function_often_use.py
@@ -1,12 +1,12 @@
-# 列表元素访问与计数
+# 列表元素访问与常用的列表对象常用的函数
 
 
-# 1.通过索引直接访问元素：如a[2]。
+# 通过索引直接访问元素：如a[2]。
 a=list(['a','b','B','c','d','e'])
 print(a[2]) # 结果：B
 #print(a[10])  # 报错：list index out of Range
 
-# 2.index函数的使用:“以值找索引”
+# 1.index函数的使用:“以值找索引”
 
 '''
 index(value,start,end)
@@ -16,16 +16,12 @@ a=list(['a','b','B','c','d','e'])
 print(a.index('a'))  # 0
 
 
-# 3.count()查询元素出现的总次数
+# 2.count()查询元素出现的总次数
 a=list(['a','b','B','c','d','e','a'])
 print(a.count('a'))
 
 
-# 4.len()查询列表长度。
+# 3.len()查询列表长度。
 a=list(['a','b','B','c','d','e'])
 print(len(a))
-
-# 5. 20 in a 关键词in来判断列表里是否有某个元素
-a=list(['a','b','B','c','d','e'])
-print('a' in a) # True
 
