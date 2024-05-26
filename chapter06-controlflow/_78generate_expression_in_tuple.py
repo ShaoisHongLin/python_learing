881103c59b6d0e74c16117c060d5461a6ec21131
shaohl <shao1417047661@gmail.com>
2024-05-26 17:31:25 +0800

    reviewed _78generate_expression_in_tuple.py

--- chapter06-controlflow/_78generate_expression_in_tuple.py
+++ chapter06-controlflow/_78generate_expression_in_tuple.py
@@ -1,6 +1,7 @@
-# 生成器推导式:(是生成器，元组如果采用推导式，只能成为生成器形式)
+# 生成器:(是生成器，元组对应的是生成器表达式，而不是推导式)
 
-# 语法：(表达式 for item in range 可迭代对象 if )
+
+# 语法：(表达式 for item in range 可迭代对象 if )，重点是小括号。
 
 tuple1=(x for x in range(1,10) if x>5)
 print(tuple1)# <generator object <genexpr> at 0x0000015A7DA6C380>
