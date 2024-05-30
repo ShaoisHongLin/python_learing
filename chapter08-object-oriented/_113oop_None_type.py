1ab5f5a1af0434163cc0520753cca0b6d86388f5
shaohl <shao1417047661@gmail.com>
2024-05-30 21:13:39 +0800

    _113oop_None_type.py

---
+++ chapter08-object-oriented/_113oop_None_type.py
@@ -0,0 +1,8 @@
+# python中与C++和JAVA不同，没有采用空值NULL而是采用None,它的None是一个专门的None对象，
+# 所有None都指向同一个None对象，它有自己的类型None-type，算作是一个特殊的常量。
+# None虽然是对象，但是不允许赋值给任何的变量。
+
+
+# 实际使用中，none在判断中作比较的返回结果永远为false
+
+# None不是False和True，如果判断序列对象为空，请别用False，而是用None
