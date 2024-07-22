56f0a49a5d4b057e42932048a56607275ffc1148
shaohl <shao1417047661@gmail.com>
2024-07-22 09:18:33 +0800

    new chapter14 -GUI

---
+++ chapter14-GUI/_156GUI--graphics-user-interfaces.py
@@ -0,0 +1,25 @@
+# GUI就是Graphics usr interfaces 图形化用户界面，是以非指令，图形化的方式让用户和电脑交互的方式。
+
+
+# 不论是什么GUI界面,背后的本质都是程序员编写过的代码。不论是你的windows的画图软件,还是office上的各种操作.
+
+
+# GUI由各个Widget构成,widget类似sandwich的后面的尾音，是英语里面小装置、小组件的意思,也就是一个个组件构成了GUI的窗口界面。
+
+# 我们在上面进行各种点击操作、都会被转化为一个个的"events"事件,与GUI进行交互.
+
+'''
+几种python支持的GUI工具
+1.Tk interface:是用于小型的GUI项目练习使用。
+(Tkinter)
+
+    tkinter是很基础的使用,如果我们学会了它,会很容易地进阶上手到大型的WxPython和PyQT等进行更高级的GUI工具的使用。
+
+
+2.WxPython是比较流行使用,可以用于大型软件项目的GUI
+
+
+3.PyQT
+
+
+'''
